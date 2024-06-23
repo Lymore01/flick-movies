e8fab94ddca1e5c80ed89cb94e3c421fc5226d7c
Lymore01 <kellytoroitich89@gmail.com>
2024-06-23 14:59:50 +0300

    updated title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Flick",
+  title: "Flick | 1.0.0",
   description: "Discover and vote for the next movie night hit!",
 };
 
